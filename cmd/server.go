c7c239d9a4df7a26a862b4d33092f6bc891422a8
leon123858 <a0970785699@gmail.com>
2025-06-06 03:52:34 +0800

    set web mode by cli

--- cmd/server.go
+++ cmd/server.go
@@ -13,9 +13,13 @@ func serverCommand() *cobra.Command {
 		Long:  `This command starts the web server for the application.`,
 		Run: func(cmd *cobra.Command, args []string) {
 			// Start the web server
-			web.Serve()
+			web.Serve(web.WebServiceConfig{
+				IsDev: cmd.Flags().Lookup("dev").Value.String() == "true",
+			})
 		},
 	}
+
+	cmd.Flags().Bool("dev", true, "Run in development mode")
 
 	return cmd
 }
--- web/server.go
+++ web/server.go
@@ -2,46 +2,68 @@ package web
 
 import (
 	"dtm/graph"
+	"dtm/mq/goch"
+	"dtm/mq/mq"
 	"dtm/mq/rabbit"
 
+	"dtm/db/db"
 	"dtm/db/mem"
 	"dtm/db/pg"
 
 	"github.com/gin-gonic/gin"
 )
 
-func Serve() {
+type WebServiceConfig struct {
+	IsDev bool
+}
+
+func Serve(config WebServiceConfig) {
 	// Setting up Gin
 	r := gin.Default()
+	// set by config
+	if config.IsDev {
+		gin.SetMode(gin.DebugMode)
+	} else {
+		gin.SetMode(gin.ReleaseMode)
+	}
+	// middle ware
 	setupMiddlewares(r)
-	// Setting up routes
+	// Setting up health check endpoint
 	r.GET("/health", func(c *gin.Context) {
 		c.JSON(200, gin.H{"status": "ok"})
 	})
 	// setup service
-	db, err := pg.InitPostgresGORM(pg.CreateDSN())
-	if err != nil {
-		panic(err)
-	}
-	defer pg.CloseGORM(db)
-	mqc := rabbit.NewRabbitConnection(rabbit.CreateAmqpURL())
-	if mqc == nil {
-		panic("Failed to connect to RabbitMQ")
-	}
-	dbDep := mem.NewInMemoryTripDBWrapper()
-	mqDep, err := rabbit.NewRabbitTripMessageQueueWrapper(mqc)
-	if err != nil {
-		panic("Failed to create RabbitMQ trip message queue wrapper: " + err.Error())
+	var dbDep db.TripDBWrapper
+	var mqDep mq.TripMessageQueueWrapper
+	if config.IsDev {
+		dbDep = mem.NewInMemoryTripDBWrapper()
+		mqDep = goch.NewGoChanTripMessageQueueWrapper()
+	} else {
+		db, err := pg.InitPostgresGORM(pg.CreateDSN())
+		if err != nil {
+			panic(err)
+		}
+		defer pg.CloseGORM(db)
+		dbDep = pg.NewPgDBWrapper(db)
+		mqc := rabbit.NewRabbitConnection(rabbit.CreateAmqpURL())
+		if mqc == nil {
+			panic("Failed to connect to RabbitMQ")
+		}
+		defer mqc.Close()
+		mqDep, err = rabbit.NewRabbitTripMessageQueueWrapper(mqc)
+		if err != nil {
+			panic("Failed to create RabbitMQ trip message queue wrapper: " + err.Error())
+		}
 	}
 	// GraphQL endpoint
-	// dbDep := pg.NewPgDBWrapper(db)
 	executableSchema := graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{
 		TripDB:                  dbDep,
 		TripMessageQueueWrapper: mqDep,
 	}})
-
+	if config.IsDev {
+		r.GET("/", GraphQLPlaygroundHandler("DTM", "/query"))
+	}
 	r.POST("/query", TripDataLoaderInjectionMiddleware(dbDep), GraphQLHandler(executableSchema))
 	r.GET("/query", TripDataLoaderInjectionMiddleware(dbDep), GraphQLHandler(executableSchema))
-	r.GET("/", GraphQLPlaygroundHandler("DTM", "/query"))
 	r.Run()
 }
